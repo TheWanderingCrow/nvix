dd65ca7657da34083a4d5bc77dd956ff37deafca
TheWanderingCrow <patrick@wanderingcrow.net>
2024-09-21 09:00:44 -0400

    Fix netrw hijacking for nvim-tree

--- config/nvim-tree.nix
+++ config/nvim-tree.nix
@@ -1,6 +1,12 @@
 {pkgs, lib, config, ...}: {
 	config = {
-		plugins.nvim-tree.enable = true;
+		plugins.nvim-tree = {
+            enable = true;
+            autoReloadOnWrite = true;
+            openOnSetup = true;
+            openOnSetupFile = true;
+            updateFocusedFile.enable = true;
+        };
 		opts.termguicolors = true;
 	};
 }
