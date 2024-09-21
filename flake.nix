5396949b3e8c6ee0d22a43d94cbc5438f467eddb
TheWanderingCrow <patrick@wanderingcrow.net>
2024-09-20 22:18:31 -0400

    Push flake

--- flake.nix
+++ flake.nix
@@ -7,5 +7,44 @@
 			url = "github:nix-community/nixvim";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
+		flake-parts = {
+			url "github:hercules-ci/flake-parts";
+			inputs.nixpkgs-lib.follows = "nixpkgs";
+		};
 	};
+
+	outputs = {
+		nixvim,
+		flake-parts,
+		...
+	} @ inputs:
+		flake-parts.lib.mkFlake {inherit inputs;} {
+			systems = [
+			   "x86_64-linux"
+			   "aarch64-linux"
+			];
+			perSystem = {
+			   pkgs,
+			   system,
+			   ...
+			}: let
+			   nixvimLib = nixvim.lib.${system};
+			   nixvim' = nixvim.legacyPackages.${system};
+			   nvim = nixvim'.makeNixvimWithModule {
+			   	inherit pkgs;
+				module = import ./config;
+			   };
+			in {
+			   checks = {
+			      default = nixvimLib.check.mkTestDerivationFromNixvimModule {
+			         inherit nvim;
+				 name = "Crow's nixvim config";
+			   };
+			   
+			   packages = {
+			   	default = nvim;
+			   };
+			};
+		    };
+		};
 }
