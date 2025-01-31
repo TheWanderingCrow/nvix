351f0f8b7b80decfe41436cb9562650517ebac40
TheWanderingCrow <contact@wanderingcrow.net>
2025-01-31 00:23:48 -0500

    only thing missing is better-escape

--- flake.nix
+++ flake.nix
@@ -1,51 +1,24 @@
 {
-	description = "Crow's nixvim configs";
+  description = "Crow's neovim configs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nvf.url = "github:notashelf/nvf";
+  };
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		nixvim = {
-			url = "github:nix-community/nixvim";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		flake-parts = {
-			url = "github:hercules-ci/flake-parts";
-			inputs.nixpkgs-lib.follows = "nixpkgs";
-		};
-	};
-
-	outputs = {
-		nixvim,
-		flake-parts,
-		...
-	} @ inputs:
-		flake-parts.lib.mkFlake {inherit inputs;} {
-			systems = [
-			   "x86_64-linux"
-			   "aarch64-linux"
-			];
-			perSystem = {
-			   pkgs,
-			   system,
-			   ...
-			}: let
-			   pkgs = import inputs.nixpkgs {
-			   	config.allowUnfree = true;
-				inherit system;
-			   };
-			   nixvimLib = nixvim.lib.${system};
-			   nixvim' = nixvim.legacyPackages.${system};
-			   nixvimModule = {
-			   	inherit pkgs;
-				module = import ./config;
-			   };
-			   nvim = nixvim'.makeNixvimWithModule nixvimModule;
-			in {
-			   checks = {
-			      default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-			   };
-			   packages = {
-			   	default = nvim;
-			   };
-			};
-		    };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    packages."x86_64-linux" = let
+      neovimConfigured = inputs.nvf.lib.neovimConfiguration {
+        inherit (nixpkgs.legacyPackages."x86_64-linux") pkgs;
+        modules = [
+          ./config.nix
+        ];
+      };
+    in {
+      default = neovimConfigured.neovim;
+    };
+  };
 }
